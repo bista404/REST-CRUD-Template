2e3c4111335b434c1d7ffa502c09d116161cece8
bista404 <67309266+bista404@users.noreply.github.com>
2021-05-25 16:33:43 +0545

    Add files via upload
    UPDATED

--- src/main/java/global/citytech/controller/EmployeeController.java
+++ src/main/java/global/citytech/controller/EmployeeController.java
@@ -1,14 +1,42 @@
 package global.citytech.controller;
 
 import global.citytech.controller.payloads.EmployeePayload;
+import global.citytech.repository.EmployeeEntity;
+import global.citytech.repository.EmployeeRepository;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+
+import javax.inject.Inject;
+
 
 @Controller("/employees")
 public class EmployeeController {
+    @Inject
+    EmployeeRepository employeeRepository;
+    @Inject
+    EmployeePayload employeePayload;
     @Get
-    public EmployeePayload get()
+    public Iterable<EmployeeEntity> getAllEmployees()
     {
-        return new EmployeePayload("1","Ram");
+        return  employeeRepository.findAll();
     }
+    @Post("/save")
+    public EmployeeEntity save(EmployeeEntity employeeEntity)
+    {
+        return employeeRepository.save(employeeEntity);
+    }
+    @Delete(value = "/{id}")
+    public void delete(String id)
+    {
+       employeeRepository.deleteById(id);
+
+    }
+
+
 }
+
+
--- src/main/java/global/citytech/repository/EmployeeRepository.java
+++ src/main/java/global/citytech/repository/EmployeeRepository.java
@@ -1,14 +1,10 @@
 package global.citytech.repository;
 
-import global.citytech.model.Employee;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
-import io.reactivex.Maybe;
-import io.reactivex.Single;
 
-import java.util.List;
 @Repository()
 @JdbcRepository(dialect = Dialect.MYSQL)
 public interface EmployeeRepository extends CrudRepository<EmployeeEntity,String> {
